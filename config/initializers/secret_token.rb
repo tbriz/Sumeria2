f1946fefa6d3de65f1c496c310018997f26e7156
Taylor Brown <taylor.w.c.brown@gmail.com>
2012-05-06 14:58:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumeria2::Application.config.secret_token = '0a35955e4df51f8ed2cf78e83477485bf8ff0f0bfe449939a7e1c497cfd162df1277c37334ed86744f91a5926cca991d2cb21ae3617038f63a413a6aced04ce5'
